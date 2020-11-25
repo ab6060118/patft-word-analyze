faa214cfd291a21d350055587f7f4dc111a10988
DaubaKao <ab6060118@gmail.com>
2020-11-25 16:02:57 +0800

    update

--- analyze.py
+++ analyze.py
@@ -7,17 +7,27 @@ import pandas as pd
 import threading as td
 from queue import Queue
 
+stop_words = [
+    'an', 'and', 'as', 'at',
+    'by',
+    'for',
+    'in', 'is',
+    'of', 'on', 'or',
+    'the', 'to']
+
 class Analyze:
     db = None
     result = None
-    classified = dict()
+    lv2 = dict()
+    lv3 = dict()
+    lv4 = dict()
 
     def __init__(self):
         self.db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="example", database="patft")
 
     def load(self):
         cursor = self.db.cursor()
-        command = 'SELECT * FROM post_v1 LIMIT 30000'
+        command = 'SELECT * FROM post_v1'
         cursor.execute(command)
         self.result = cursor.fetchall()
 
@@ -25,21 +35,56 @@ class Analyze:
         for row in self.result:
             classes = re.findall(r"\b[A-Z]\d\d[A-Z]\b", row[4])
             for cls in classes:
-                if cls not in self.classified:
-                    self.classified[cls] = set()
-                self.classified[cls].add(row)
-        print(len(self.classified))
+                clsA = re.findall(r".", cls)[0]
+                clsB = re.findall(r"[A-Z]\d\d", cls)[0]
+
+                if clsA not in self.lv2:
+                    self.lv2[clsA] = set()
+                self.lv2[clsA].add(row[0])
+
+                if clsB not in self.lv3:
+                    self.lv3[clsB] = set()
+                self.lv3[clsB].add(row[0])
+
+                if cls not in self.lv4:
+                    self.lv4[cls] = set()
+                self.lv4[cls].add(row)
+
+        i = 0
+        j = 0
+        k = 0
+        #  for key, posts in self.lv2.items():
+            #  print(key, len(posts))
+            #  i = i + len(posts)
+
+        #  for key, posts in self.lv3.items():
+            #  print(key, len(posts))
+            #  j = j + len(posts)
+
+        for key, posts in self.lv4.items():
+            print(key, ',', len(posts))
+            k = k + len(posts)
+        print(i, j, k)
 
     def doAnalyze(self):
         for key, posts in self.classified.items():
-            print(key, 'start', len(posts))
-            vectorizer = TfidfVectorizer(stop_words=None, token_pattern="(?u)\\b\\w\\w+\\b")
+            vectorizer = TfidfVectorizer(stop_words=stop_words, token_pattern="(?u)\\b\\w\\w+\\b")
             X = vectorizer.fit_transform(list(map(lambda post: ' '.join([post[2], post[3], post[6]]), posts)))
-            r = pd.DataFrame(X.toarray(),columns=vectorizer.get_feature_names(), index=list(map(lambda p: p[0], posts)))
-            print(key, 'done')
+            def mapfn(a1):
+                zipped = list(zip(vectorizer.get_feature_names(), a1))
+                res = sorted(zipped, key = lambda x: x[1])
+                a, b = zip(*res[-10:])
+                return a
+            mapped = list(map(mapfn, X.toarray()))
+            r = pd.DataFrame(mapped, columns=list(map(lambda p: p, range(1, 11))), index=list(map(lambda x: x[0], posts)))
+            print(key, )
+            for i in range(len(mapped)):
+                print(list(posts)[i][0], mapped[i])
+            #  print(r)
+            print()
 
 analyzer = Analyze()
 
 analyzer.load()
 analyzer.classification()
-analyzer.doAnalyze()
+#  analyzer.doAnalyze()
