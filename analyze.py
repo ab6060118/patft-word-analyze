892ea6b95787dfb37fa79272971ec4aa6b847019
DaubaKao <ab6060118@gmail.com>
2020-11-30 22:15:20 +0800

    update

--- analyze.py
+++ analyze.py
@@ -22,13 +22,14 @@ class Analyze:
     lv3 = dict()
     lv4 = dict()
     lv5 = dict()
+    appearInDoc = dict()
 
     def __init__(self):
         self.db = MySQLdb.connect(host="127.0.0.1", user="root", passwd="example", database="patft")
 
     def load(self):
         cursor = self.db.cursor()
-        #  command = 'SELECT * FROM post_v1 LIMIT 50'
+        #  command = 'SELECT * FROM post_v1 LIMIT 500'
         command = 'SELECT * FROM post_v1'
         cursor.execute(command)
         self.result = cursor.fetchall()
@@ -91,7 +92,17 @@ class Analyze:
         #  X = vectorizer.fit_transform(list(map(lambda post: ' '.join([post[2], post[3], post[6]]), posts)))
         # claim
         X = vectorizer.fit_transform(list(map(lambda post: ' '.join([post[3]]), posts)))
-        Z = transformer.fit_transform(X)
+        #  Z = transformer.fit_transform(X)
+
+        def appearInDocMap(a1):
+            a2 = vectorizer.get_feature_names()
+            i = 0
+            for a in a1:
+                if a > 0:
+                    if a2[i] not in self.appearInDoc:
+                        self.appearInDoc[a2[i]] = 0
+                    self.appearInDoc[a2[i]] = self.appearInDoc[a2[i]] + 1
+                i = i + 1
 
         #word count
         def mapfn(a1, a2):
@@ -106,26 +117,33 @@ class Analyze:
             res = sorted(zipped, reverse=True, key = lambda x: x[1])
             a, b = zip(*res[:10])
             return a
+        #appear in doc
+
         #word count
-        mapped = list(map(mapfn, Z.toarray(), X.toarray()))
+        #  mapped = list(map(mapfn, Z.toarray(), X.toarray()))
         #TFIDF
         #  mapped = list(map(mapfn1, Z.toarray(), X.toarray()))
-        print(key)
-        print('id, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10')
-        for i in range(len(mapped)):
-            print(
-                list(posts)[i][0].replace(',', '') + ',',
-                mapped[i][0] + ',',
-                mapped[i][1] + ',',
-                mapped[i][2] + ',',
-                mapped[i][3] + ',',
-                mapped[i][4] + ',',
-                mapped[i][5] + ',',
-                mapped[i][6] + ',',
-                mapped[i][7] + ',',
-                mapped[i][8] + ',',
-                mapped[i][9] + ','
-            )
+        #  print(key)
+        #  print('id, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10')
+        #  for i in range(len(mapped)):
+            #  print(
+                #  list(posts)[i][0].replace(',', '') + ',',
+                #  mapped[i][0] + ',',
+                #  mapped[i][1] + ',',
+                #  mapped[i][2] + ',',
+                #  mapped[i][3] + ',',
+                #  mapped[i][4] + ',',
+                #  mapped[i][5] + ',',
+                #  mapped[i][6] + ',',
+                #  mapped[i][7] + ',',
+                #  mapped[i][8] + ',',
+                #  mapped[i][9] + ','
+            #  )
+        for post in X.toarray():
+            appearInDocMap(post)
+
+        for key, count in self.appearInDoc.items():
+            print(key + ',' + str(count))
 
 analyzer = Analyze()
 
